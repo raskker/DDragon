dcedce2ede4b8caed54766d1c53d4686c316243a
jdrabant <jakob-drabant>
2021-06-15 23:10:53 +0200

    router

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,15 +1,12 @@
 <template>
-<section class="p-5">
+<section class="">
   <div class="container">
     <div class="row">
-      <div class="col-md m-3" v-for="c in champions" v-bind:key="c.key">
+      <div class="col-md-2 m-3" v-for="c in champions" v-bind:key="c.key">
         <div class="card bg-dark text-light">
           <div class="card-body text-center" >
             <img :src="c.icon" alt="">
             <h3 class="card-title mb-3">{{ c.name }}</h3>
-            <p class="card-text pb-2">
-              {{c.description}}
-            </p>
             <a href="" class="btn btn-primary">Learn More</a>
           </div>
         </div>
@@ -55,6 +52,6 @@ a {
 }
 
 .card {
-  min-height: 400px;
+  /* min-height: 200px; */
 }
 </style>
