fd5fc4f8c08a19dbec0b937ab742379fa50bec67
jdrabant <jakob-drabant>
2021-06-17 23:09:55 +0200

    Work with CommunityDargon CDNs -> more infos +skins pics

--- src/components/ChampionDetails.vue
+++ src/components/ChampionDetails.vue
@@ -1,8 +1,12 @@
 <template>
     <div class="container">
         <div class="jumbotron">
-            <h1>{{champs.name}}</h1>
-            <p>{{champs.description}}</p>
+            <h1>{{champ.name}}</h1>
+            <h4>{{champ.title}}</h4>
+            <p>{{champ.shortBio}}</p>
+        <div v-for="s in champ.skins" v-bind:key="s.splashPath">
+            <img class="col-md-4" :src="url(s.splashPath)" alt="">
+        </div>
         </div>
     </div>
 </template>
@@ -12,12 +16,29 @@ export default {
   data() {
     return {
       champName: this.$route.params.name,
+      champ: {},
     };
   },
-  computed: {
-    champs() {
-      return this.$store.state.champions.find((c) => c.name === this.champName);
+  methods: {
+    url(path) {
+      var offset = path.substring(path.lastIndexOf("assets") + 7);
+      return (
+        "https://raw.communitydragon.org/11.12/plugins/rcp-be-lol-game-data/global/default/" +
+        offset
+      );
     },
+  },
+  computed: {},
+
+  mounted() {
+    fetch(
+      "https://cdn.communitydragon.org/11.12.1/champion/" +
+        this.champName.toLowerCase() +
+        "/data"
+    )
+      .then((res) => res.json())
+      .then((data) => (this.champ = data))
+      .catch((err) => console.log(err.message));
   },
 };
 </script>
