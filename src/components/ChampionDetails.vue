e30061c3fbe1e25ac345c4f6ff3c045d7ebd17fa
jdrabant <jakob-drabant>
2021-06-21 22:57:01 +0200

    json new champs + spells

--- src/components/ChampionDetails.vue
+++ src/components/ChampionDetails.vue
@@ -3,10 +3,12 @@
             <h1>{{champ.name}}</h1>
             <h4>{{champ.title}}</h4>
             <p>{{champ.shortBio}}</p>
-            <p> {{passive.name}}</p>
-            <p> {{passive.description}}</p>
-           
-            <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
+              <div id="carouselExampleControls" class="carousel slide mb-5" data-bs-ride="carousel">
+                <!-- <ol class="carousel-indicators">
+                  <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+                  <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+                    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+  </ol> -->
               <div class="carousel-inner">
                 <div class="carousel-item" :class="{ active: s.id==splashId }" v-for="s in champ.skins" v-bind:key="s.id">
                   <img class="d-block w-100" :src="url(s.splashPath)" alt="">
@@ -24,6 +26,18 @@
                 <span class="visually-hidden">Next</span>
               </button>
             </div>
+            
+            <p> {{passive.name}}</p>
+            <img class="" :src="urlPassiv()" alt="">
+            <p> {{passive.description}}</p>
+            <div v-for="s in champ.spells" v-bind:key="s.spellKey">
+              
+              <p> {{s.name}}</p>
+              <img class="" :src="urlSpells(s.spellKey)" alt="">
+              <p> {{s.description}}</p>
+            </div>
+           
+
         
         
     </div>
@@ -45,6 +59,21 @@ export default {
       return (
         "https://raw.communitydragon.org/11.12/plugins/rcp-be-lol-game-data/global/default/" +
         offset
+      );
+    },
+    urlPassiv() {
+      return (
+        "https://cdn.communitydragon.org/11.12.1/champion/" +
+        this.champ.id +
+        "/ability-icon/passive"
+      );
+    },
+    urlSpells(spell) {
+      return (
+        "https://cdn.communitydragon.org/11.12.1/champion/" +
+        this.champ.id +
+        "/ability-icon/" +
+        spell
       );
     },
   },
