c33a54a364fdd99e2e4b1f804e466e32ea78e4f9
jdrabant <jakob-drabant>
2021-07-02 23:17:02 +0200

    champion cards + searchbar css

--- src/components/Champions.vue
+++ src/components/Champions.vue
@@ -1,7 +1,10 @@
 <template>
 <section class="">
   <div class="container-fluid">
-     <div class="input-group">
+    <div class="championHeadline justify-content-center">
+  Champions
+</div>
+     <div class="searchBar input-group justify-content-center">
   <div class="form-floating">
     <input id="form1" type="search" v-model="search" class="form-control" placeholder="search">
     <label class="form-label" for="form1">Search</label>
@@ -9,13 +12,22 @@
   <button type="button" class="btn btn-primary">
     <fa icon="search"></fa>
   </button>
+  
 </div>
+
     <div class="row justify-content-center">
-      <div class="col-md-2 m-3" v-for="c in filteredChamps" v-bind:key="c.key">
-        <div class="card bg-dark text-light">
-          <div class="card-body text-center" >
-            <img :src="c.icon" alt="">
-            <h3 class="card-title mb-3">{{ c.name }}</h3>
+      <div class="col-md-2 m-3 " v-for="c in filteredChamps" v-bind:key="c.key">
+        <div class="card">
+          <img
+            :src="c.icon"
+            class="card-img-top"
+            alt="..."
+          />
+          <div class="card-body">
+            <h5 class="card-title">{{ c.name }}</h5>
+            <p class="card-text">
+              {{c.title}}
+            </p>
             <router-link class="btn btn-primary" :to="{ name: 'ChampionDetails' , params: {name:c.id}}">Learn More</router-link>
           </div>
         </div>
@@ -47,5 +59,21 @@ export default {
 <style scoped>
 h3 {
   margin: 40px 0 0;
+}
+
+.card {
+  width: 200px;
+  margin: 0 auto;
+  margin-top: 20px;
+}
+
+.searchBar {
+  margin-top: 30px;
+  margin-bottom: 20px;
+}
+
+.championHeadline {
+  font-size: 50px;
+  margin-top: 20px;
 }
 </style>
